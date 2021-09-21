a96a8a5518bd246c03dd27d5c6e6a8d1f87d4c6a
LeoFernanndes <balbino.leonel@gmail.com>
2021-09-21 00:08:46 -0300

    fix: products model
    altered:
      - rename and removal of table columns to better match the crm api sample

--- products/models.py
+++ products/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    category = models.CharField(max_length=255, null=False)
-    product_name = models.CharField(max_length=255, unique=True, null=False)
-    unit_value = models.FloatField(null=False)
-    quantity = models.IntegerField(null=False)
+    type = models.CharField(max_length=255, null=False)
+    value = models.FloatField(null=False)
+    qty = models.IntegerField(null=False)
 
